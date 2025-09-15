5eb5554ca7f8aeea80c9311f3fd42f3550d42d51
[valentin563] <[valentin.poinet@etu.unilim.fr]>
2025-09-15 10:59:55 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [tailwindcss()],
-    base: "/<TD5>",
+    // base: "/<TD5>",
 });
